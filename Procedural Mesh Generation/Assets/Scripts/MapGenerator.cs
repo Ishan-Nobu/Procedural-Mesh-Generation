de746c448a296110b57d420bd416cbb061ecd2d3
IshanShino <ishans1045@gmail.com>
2024-07-17 15:15:09 +0530

    Some Bug Fixes

--- Procedural Mesh Generation/Assets/Scripts/MapGenerator.cs
+++ Procedural Mesh Generation/Assets/Scripts/MapGenerator.cs
@@ -23,6 +23,11 @@ public class MapGenerator : MonoBehaviour
 	Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
 	Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
 
+	void Awake() 
+	{	
+		textureData.ApplyToMaterial(terrainMaterial);
+		textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+	}	
 	void OnValuesUpdated()
 	{
 		if (!Application.isPlaying)
@@ -49,7 +54,9 @@ public class MapGenerator : MonoBehaviour
 		}
 	}
 	public void DrawMapInEditor() 
-    {
+    {	
+		textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+
 		MapData mapData = GenerateMapData (Vector2.zero);
 
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
@@ -151,8 +158,6 @@ public class MapGenerator : MonoBehaviour
 				}
 			}
 		}
-		textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
-
 		return new MapData (noiseMap);
 	}
 
